128fc59851019b2cc0a13b63f231d1ffabf67f2e
Kaloyan Minchev <54272165+kaloyanminchev@users.noreply.github.com>
2019-09-10 12:35:56 +0300

    Add files via upload

---
+++ Defining Classes - Lab/BankAccount/BankAccount.java
@@ -0,0 +1,32 @@
+package DefiningClasses.BankAccount;
+
+public class BankAccount {
+
+    private static final double RATE_DEFAULT_VALUE = 0.02d;
+    private static int idCounter = 1;
+    private static double interestRate = RATE_DEFAULT_VALUE;
+
+    private int id;
+    private double balance;
+
+    public BankAccount() {
+        this.id = idCounter++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static void setInterestRate(double interest) {
+        BankAccount.interestRate = interest;
+    }
+
+    public double getInterest(int years) {
+        return BankAccount.interestRate * years * this.balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+}
